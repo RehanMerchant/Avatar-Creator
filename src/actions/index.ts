bb2d417812977370e6af634fe5a218215b5e3f41
Rehan3000 <skmdrehan64@gmail.com>
2024-11-18 12:35:12 +0530

    bug fix

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,261 +1,255 @@
+export class Global {
+  static framey = 10;
+  static framex = 0;
 
- export class Global {
-
-    static framey = 10
-    static framex = 0
-
-    static getGlobalValue():number{
-        return this.framex,this.framey
-    }
-
-    static setGlobalValue(y:number): void {
-        this.framey = y;
-      }
-
- }
-
-
-
-
-
- export class BaseBody extends Global {
-    imagebody: any;
-    imagehead: any;
-    imagetail:any;
-    imagewings:any;
-    prosthesis:any;
-    prosthesismask:any;
-    imagenose:any;
-    imagehorns:any;
-    imagehair:any;
-    imageeyes:any;
-    imageeyebrow:any;
-    animation: number;
-    ctx: CanvasRenderingContext2D;
-    SpriteSides = 64;
-    imageear: any;
-    
-    constructor(imagebody:any,imagehead:any,imagetail:any,imagewings:any,prosthesis:any,prosthesismask:any,imageear:any,imagenose:any,imagehorns:any,imagehair:any,imageeyes:any,imageeyebrow:any,animation:number,ctx:CanvasRenderingContext2D){
-        super()
-        this.imagebody = imagebody;
-        this.imagehead = imagehead;
-        this.imagetail = imagetail;
-        this.imagehorns=imagehorns;
-        this.imagewings = imagewings;
-        this.prosthesis = prosthesis;
-        this.prosthesismask = prosthesismask;
-        this.imageear = imageear;
-        this.imagenose=imagenose;
-        this.imagehair = imagehair;
-        this.imageeyes = imageeyes;
-        this.imageeyebrow = imageeyebrow;
-        this.animation = animation;
-        this.ctx = ctx;
+  static getGlobalValue(): number {
+    return this.framex, this.framey;
   }
-  getGlobalValue():number {
-    return Global.framex, Global.framey
+
+  static setGlobalValue(y: number): void {
+    this.framey = y;
+  }
 }
 
+export class BaseBody extends Global {
+  imagebody: any;
+  imagehead: any;
+  imagetail: any;
+  imagewings: any;
+  prosthesis: any;
+  prosthesismask: any;
+  imagenose: any;
+  imagehorns: any;
+  imagehair: any;
+  imageeyes: any;
+  imageeyebrow: any;
+  animation: number;
+  ctx: CanvasRenderingContext2D;
+  SpriteSides = 64;
+  imageear: any;
 
-   draw(){
-    this.ctx.clearRect(0,0,this.SpriteSides,this.SpriteSides)
-    this.ctx.drawImage(
-        this.imagetail,
-        this.SpriteSides*BaseBody.framex,
-        this.SpriteSides*BaseBody.framey,  
-        this.SpriteSides,
-        this.SpriteSides,
-        0,
-        0,
-        this.SpriteSides,
-        this.SpriteSides
-        )
-        this.ctx.drawImage(
-            this.imagewings,
-            this.SpriteSides*BaseBody.framex,
-            this.SpriteSides*BaseBody.framey,  
-            this.SpriteSides,
-            this.SpriteSides,
-            0,
-            0,
-            this.SpriteSides,
-            this.SpriteSides
-            )
-
-    this.ctx.drawImage(
-    this.imagebody,
-    this.SpriteSides*BaseBody.framex,
-    this.SpriteSides*BaseBody.framey,
-    this.SpriteSides,
-    this.SpriteSides,
-    0,
-    0,
-    this.SpriteSides,
-    this.SpriteSides
-    )
- 
-    this.ctx.drawImage(
-        this.imagehead,
-        this.SpriteSides*BaseBody.framex,
-        this.SpriteSides*BaseBody.framey,
-        this.SpriteSides,
-        this.SpriteSides,
-        0,
-        0,
-        this.SpriteSides,
-        this.SpriteSides
-        )
-        
-        this.ctx.drawImage(
-            this.prosthesismask,
-            this.SpriteSides*BaseBody.framex,
-            this.SpriteSides*BaseBody.framey,
-            this.SpriteSides,
-            this.SpriteSides,
-            0,
-            0,
-            this.SpriteSides,
-            this.SpriteSides
-            )
-        this.ctx.drawImage(
-            this.prosthesis,
-            this.SpriteSides*BaseBody.framex,
-            this.SpriteSides*BaseBody.framey,
-            this.SpriteSides,
-            this.SpriteSides,
-            0,
-            0,
-            this.SpriteSides,
-            this.SpriteSides
-            )
-            this.ctx.drawImage(
-                this.imageear,
-                this.SpriteSides*BaseBody.framex,
-                this.SpriteSides*BaseBody.framey,  
-                this.SpriteSides,
-                this.SpriteSides,
-                0,
-                0,
-                this.SpriteSides,
-                this.SpriteSides
-                )
-                this.ctx.drawImage(
-                    this.imagenose,
-                    this.SpriteSides*BaseBody.framex,
-                    this.SpriteSides*BaseBody.framey,  
-                    this.SpriteSides,
-                    this.SpriteSides,
-                    0,
-                    0,
-                    this.SpriteSides,
-                    this.SpriteSides
-                    )
-                    this.ctx.drawImage(
-                        this.imageeyes,
-                        this.SpriteSides*BaseBody.framex,
-                        this.SpriteSides*BaseBody.framey,  
-                        this.SpriteSides,
-                        this.SpriteSides,
-                        0,
-                        0,
-                        this.SpriteSides,
-                        this.SpriteSides
-                        )
-                        this.ctx.drawImage(
-                            this.imageeyebrow,
-                            this.SpriteSides*BaseBody.framex,
-                            this.SpriteSides*BaseBody.framey,  
-                            this.SpriteSides,
-                            this.SpriteSides,
-                            0,
-                            0,
-                            this.SpriteSides,
-                            this.SpriteSides
-                            )
-                
-                    this.ctx.drawImage(
-                        this.imagehair,
-                        this.SpriteSides*BaseBody.framex,
-                        this.SpriteSides*BaseBody.framey,  
-                        this.SpriteSides,
-                        this.SpriteSides,
-                        0,
-                        0,
-                        this.SpriteSides,
-                        this.SpriteSides
-                        )
-                                    this.ctx.drawImage(
-                                        this.imagehorns,
-                                        this.SpriteSides*ForegroundLayer.framex,
-                                        this.SpriteSides*ForegroundLayer.framey,
-                                        this.SpriteSides,
-                                        this.SpriteSides,
-                                        0,
-                                        0,
-                                        this.SpriteSides,
-                                        this.SpriteSides
-                                        )
- 
-   }
-    
- }
-
- 
-
-
- 
-  
-
- export class ForegroundLayer extends Global {
-    imagetailfg:any;
-    imagewingsfg:any;
-
-    animation: number;
-    ctx: CanvasRenderingContext2D;
-    SpriteSides = 64;
-    constructor(imagetailfg:any,imagewingsfg:any,animation:number,ctx:CanvasRenderingContext2D){
-        super()
-        this.imagetailfg = imagetailfg;
-        this.imagewingsfg = imagewingsfg;
-        this.animation = animation
-        this.ctx = ctx
+  constructor(
+    imagebody: any,
+    imagehead: any,
+    imagetail: any,
+    imagewings: any,
+    prosthesis: any,
+    prosthesismask: any,
+    imageear: any,
+    imagenose: any,
+    imagehorns: any,
+    imagehair: any,
+    imageeyes: any,
+    imageeyebrow: any,
+    animation: number,
+    ctx: CanvasRenderingContext2D
+  ) {
+    super();
+    this.imagebody = imagebody;
+    this.imagehead = imagehead;
+    this.imagetail = imagetail;
+    this.imagehorns = imagehorns;
+    this.imagewings = imagewings;
+    this.prosthesis = prosthesis;
+    this.prosthesismask = prosthesismask;
+    this.imageear = imageear;
+    this.imagenose = imagenose;
+    this.imagehair = imagehair;
+    this.imageeyes = imageeyes;
+    this.imageeyebrow = imageeyebrow;
+    this.animation = animation;
+    this.ctx = ctx;
   }
-  getGlobalValue():number {
-    return Global.framex, Global.framey
-
+  getGlobalValue(): number {
+    return Global.framex, Global.framey;
   }
-      
-   draw(){
-   
+
+  draw() {
+    this.ctx.clearRect(0, 0, this.SpriteSides, this.SpriteSides);
     this.ctx.drawImage(
-        this.imagewingsfg,
-        this.SpriteSides*ForegroundLayer.framex,
-        this.SpriteSides*ForegroundLayer.framey,
-        this.SpriteSides,
-        this.SpriteSides,
-        0,
-        0,
-        this.SpriteSides,
-        this.SpriteSides
-        )
+      this.imagetail,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
     this.ctx.drawImage(
-        this.imagetailfg,
-        this.SpriteSides*ForegroundLayer.framex,
-        this.SpriteSides*ForegroundLayer.framey,
-        this.SpriteSides,
-        this.SpriteSides,
-        0,
-        0,
-        this.SpriteSides,
-        this.SpriteSides
-        )
-      
+      this.imagewings,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
 
-   }
-    
- }
+    this.ctx.drawImage(
+      this.imagebody,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
 
- 
+    this.ctx.drawImage(
+      this.imagehead,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
 
+    this.ctx.drawImage(
+      this.prosthesismask,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
+    this.ctx.drawImage(
+      this.prosthesis,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
+    this.ctx.drawImage(
+      this.imageear,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
+    this.ctx.drawImage(
+      this.imagenose,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
+    this.ctx.drawImage(
+      this.imageeyes,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
+    this.ctx.drawImage(
+      this.imageeyebrow,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
 
- 
+    this.ctx.drawImage(
+      this.imagehair,
+      this.SpriteSides * BaseBody.framex,
+      this.SpriteSides * BaseBody.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
+    this.ctx.drawImage(
+      this.imagehorns,
+      this.SpriteSides * ForegroundLayer.framex,
+      this.SpriteSides * ForegroundLayer.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
+  }
+}
+
+export class ForegroundLayer extends Global {
+  imagetailfg: any;
+  imagewingsfg: any;
+
+  animation: number;
+  ctx: CanvasRenderingContext2D;
+  SpriteSides = 64;
+  constructor(
+    imagetailfg: any,
+    imagewingsfg: any,
+    animation: number,
+    ctx: CanvasRenderingContext2D
+  ) {
+    super();
+    this.imagetailfg = imagetailfg;
+    this.imagewingsfg = imagewingsfg;
+    this.animation = animation;
+    this.ctx = ctx;
+  }
+  getGlobalValue(): number {
+    return Global.framex, Global.framey;
+  }
+
+  draw() {
+    this.ctx.drawImage(
+      this.imagewingsfg,
+      this.SpriteSides * ForegroundLayer.framex,
+      this.SpriteSides * ForegroundLayer.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
+    this.ctx.drawImage(
+      this.imagetailfg,
+      this.SpriteSides * ForegroundLayer.framex,
+      this.SpriteSides * ForegroundLayer.framey,
+      this.SpriteSides,
+      this.SpriteSides,
+      0,
+      0,
+      this.SpriteSides,
+      this.SpriteSides
+    );
+  }
+}
